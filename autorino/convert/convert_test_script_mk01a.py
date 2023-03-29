319e51c131bc970b25f14242afd468d5b1a892f0
Pierre Sakic <sakic@ipgp.fr>
2023-03-29 11:11:27 +0000

    mods  test script

--- autorino/convert/convert_test_script_mk01a.py
+++ autorino/convert/convert_test_script_mk01a.py
@@ -22,43 +22,54 @@ outdir_rinexmoded = "/scratch/convgnss/010_tests_autorino_converters/rinexmoded"
 utils.create_dir(outdir_rinexmoded)
 utils.create_dir(outdir_converted)
 
-conv_trm2rinex=False
+conv_trm2rinex=1 
 conv_trm_runpkr00=0
 conv_trm_teqc=0
 conv_leica_mdb2rinex=0
-conv_sept_sbf2rin=1
+conv_sept_sbf2rin=0
 verbose=False
 
+nmax = 10
 
-sitelogs = rinexmod_api._sitelog_input_manage(psitelogs,force=False)
+sitelogs = rinexmod_api.sitelog_input_manage(psitelogs,force=False)
 
 #### TRIMBLE
 if conv_trm2rinex:
-    for fraw in utils.find_recursive(p,"*T02"):
+    flist = utils.find_recursive(p,"*T02")
+    flist = flist[:nmax]
+    for fraw in flist:
         frnxtmp, _ = cv.converter_run(fraw, outdir_converted, converter = 'trm2rinex')
         rinexmod_api.rinexmod(frnxtmp,outdir_rinexmoded,sitelog=sitelogs,force_rnx_load=True,verbose=verbose)
 
 #### TRIMBLE RUNPKR00
 if conv_trm_runpkr00:
-    for fraw in utils.find_recursive(p,"*T02"):
+    flist = utils.find_recursive(p,"*T02")
+    flist = flist[:nmax]
+    for fraw in flist:
         cv.converter_run(fraw, outdir_converted, converter = 'runpkr00')
         rinexmod_api.rinexmod(frnxtmp,outdir_rinexmoded,sitelog=sitelogs,force_rnx_load=True,verbose=verbose)
 
 #### TRIMBLE TEQC
 if conv_trm_teqc:
-    for fraw in utils.find_recursive(outdir_converted,"*tgd"):
+    flist = utils.find_recursive(outdir_converted,"*tgd")
+    flist = flist[:nmax]
+    for fraw in flist:
         frnxtmp, _ = cv.converter_run(fraw, outdir_converted, converter = 'teqc')
         rinexmod_api.rinexmod(frnxtmp,outdir_rinexmoded,sitelog=sitelogs,force_rnx_load=True,verbose=verbose)
 
 #### MDB2RINEX
 if conv_leica_mdb2rinex:
-    for fraw in utils.find_recursive(p,"*m00"):
+    flist = utils.find_recursive(p,"*m00")
+    flist = flist[:nmax]
+    for fraw in flist:
         frnxtmp, _ = cv.converter_run(fraw, outdir_converted, converter = 'mdb2rinex')
         rinexmod_api.rinexmod(frnxtmp,outdir_rinexmoded,sitelog=sitelogs,force_rnx_load=True,verbose=verbose)
 
 #### SBF2RIN
 if conv_sept_sbf2rin:
-    for fraw in utils.find_recursive(p,"*_"):
+    flist = utils.find_recursive(p,"*_")
+    flist = flist[:nmax]
+    for fraw in flist:
         frnxtmp, _ = cv.converter_run(fraw, outdir_converted, converter = 'sbf2rin')    
         rinexmod_api.rinexmod(frnxtmp,outdir_rinexmoded,sitelog=sitelogs,force_rnx_load=True,verbose=verbose)
 
