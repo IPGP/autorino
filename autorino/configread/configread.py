4103c02b4cab058f4da1d4fc8f6a5ead1de69350
PSTPX1 <psakicki@TPX1-GFZ>
2024-01-10 22:52:03 +0100

    recreate configread

---
+++ autorino/configread/configread.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 15:47:05 2022
+
+@author: psakicki
+"""
+
+import os
+from autorino import general as arogen
+from autorino import download as arodwl
+
+import yaml
+
+
+
+def _translator_epoch(path_inp,epoch_inp):
+    """
+    set the correct epoch in path_input string the with the strftime aliases
+    https://docs.python.org/3/library/datetime.html#strftime-and-strptime-behavior
+    """
+    path_translated = str(path_inp)
+    path_translated = epoch_inp.strftime(path_translated)
+    return path_translated
+
+def _translator_keywords(path_inp,translator_dict):
+    """
+    """
+    path_translated = str(path_inp)
+    for k,v in translator_dict.items():
+        path_translated = path_translated.replace("<"+k+">",v)
+    return path_translated
+    
+def translator(path_inp,epoch_inp=None,translator_dict=None):
+    path_translated = str(path_inp)
+    if epoch_inp:
+        path_translated = _translator_epoch(path_translated,epoch_inp)
+    if translator_dict:
+        path_translated = _translator_keywords(path_translated,translator_dict)
+    return path_translated
+
+
+def session_download_from_configfile(configfile_path):
+    
+    y1 = yaml.safe_load(open(configfile_path))
+    
+    ystat = y1["station"]
+
+    protocol = ystat["access"]["protocol"]
+    hostname = ystat["access"]["hostname"]
+    sta_user = ystat["access"]["login"]
+    sta_pass = ystat["access"]["password"]
+    site = ystat["site"]    
+    
+    ysess_lst = ystat["sessions_list"]
+    
+    sess_stk, dwnld_stk = [], []
+    
+    for yses_i in ysess_lst:
+        ########### session
+        yses = yses_i["session"]
+        name = yses["name"]
+        
+        session_period = yses["file_period"]
+        remote_dir = yses["remote_dir"]
+        tmp_dir = yses["tmp_dir"]
+        remote_fname = yses["remote_fname"]    
+        
+        sess = arogen.SessionGnss(name = name,
+                                protocol = protocol,
+                                remote_dir=remote_dir,
+                                tmp_dir=tmp_dir,
+                                hostname=hostname,
+                                sta_user=sta_user,
+                                sta_pass=sta_pass,
+                                site=site,
+                                session_period=session_period,
+                                remote_fname=remote_fname)
+        
+        sess_stk.append(sess)
+
+        ##### Epoch rang
+        Yepochrang = yses_i["epoch_range"]        
+        rang = arogen.EpochRange(Yepochrang["epoch1"],
+                                 Yepochrang["epoch2"],
+                                 session_period)
+
+        ############ Workflow
+        ##### Download
+        Ywfl = yses_i["workflow"]
+        ydwnld = Ywfl["download"]
+        
+        output_dir_parent = ydwnld["output_dir_parent"] 
+        output_dir_struture = ydwnld["output_dir_structure"] 
+        output_path = os.path.join(output_dir_parent,
+                                   output_dir_struture)
+       
+        dwnld = arodwl.DownloadGnss(sess,rang,output_path)
+        
+        dwnld_stk.append(dwnld)
+        
+    # if len(sess_stk) < 2:
+        # return sess_stk[0], req_stk[0]
+    # else:
+    return sess_stk, dwnld_stk
