0dad6481e82f7e506c6938b531a50c19e331325d
sysop@PitonHudson <sysop@PitonHudson.ipgp.fr>
2024-01-25 10:57:18 +0000

    stuffs on hudson

--- autorino/configread/configread.py
+++ autorino/configread/configread.py
@@ -9,6 +9,8 @@ Created on Thu Dec  1 15:47:05 2022
 import os
 #import autorino.general as arogen
 import autorino.download as arodwl
+import autorino.session as aroses
+import autorino.epochrange as aroepo
 
 import yaml
 
@@ -38,22 +40,22 @@ def session_download_from_configfile(configfile_path):
         tmp_dir = yses["tmp_dir"]
         remote_fname = yses["remote_fname"]    
         
-        sess = arogen.SessionGnss(name = name,
-                                protocol = protocol,
-                                remote_dir=remote_dir,
-                                tmp_dir=tmp_dir,
-                                hostname=hostname,
-                                sta_user=sta_user,
-                                sta_pass=sta_pass,
-                                site=site,
-                                session_period=session_period,
-                                remote_fname=remote_fname)
+        sess = aroses.SessionGnss(name = name,
+                                  protocol = protocol,
+                                  remote_dir=remote_dir,
+                                  tmp_dir=tmp_dir,
+                                  hostname=hostname,
+                                  sta_user=sta_user,
+                                  sta_pass=sta_pass,
+                                  site=site,
+                                  session_period=session_period,
+                                  remote_fname=remote_fname)
         
         sess_stk.append(sess)
 
         ##### Epoch rang
         Yepochrang = yses_i["epoch_range"]        
-        rang = arogen.EpochRange(Yepochrang["epoch1"],
+        rang = aroepo.EpochRange(Yepochrang["epoch1"],
                                  Yepochrang["epoch2"],
                                  session_period)
 
