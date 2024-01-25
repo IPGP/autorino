5dc1c5618033b9820ec06594e5c40350973b0b80
PSTPX1 <psakicki@TPX1-GFZ>
2024-01-25 19:24:12 +0100

    refactor

--- autorino/handle/handle.py
+++ autorino/handle/handle.py
@@ -6,14 +6,13 @@ Created on Wed Jan 10 15:00:40 2024
 @author: psakic
 """
 
-import autorino.workflow as arowkf
 import autorino.general  as arogen
 
 # Create a logger object.
 import logging
 logger = logging.getLogger(__name__)
 
-class HandleGnss(arogen.WorkflowGnss):
+class HandleGnss(arogen.StepGnss):
     def __init__(self,session,epoch_range,out_dir):
         super().__init__(session,epoch_range,out_dir)
         
