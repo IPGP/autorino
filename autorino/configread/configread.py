8c49bc821c9183bb8bdbbb2da353f49f34d9b6ce
PSTPX1 <psakicki@TPX1-GFZ>
2024-01-26 18:19:20 +0100

    ready read_configfile fct

--- autorino/configread/configread.py
+++ autorino/configread/configread.py
@@ -15,77 +15,67 @@ import autorino.convert as arocnv
 #import autorino.epochrange as aroepo
 
 import yaml
-
-
-#def read_configfile(configfile_path):
     
-configfile_path = "/home/psakicki/CODES/IPGP/autorino/configfiles/proto_config_06a_RVAG.yml"
-
-y = yaml.safe_load(open(configfile_path))
-
-y_station = y["station"]
-
-y_site = y_station["site"]
-y_access = y_station["access"]
-
-y_sessions_list = y_station["sessions_list"]
-
-for yses in y_sessions_list:
-    tmp_dir = os.path.join(yses['session']['tmp_dir_parent'],
-                           yses['session']['tmp_dir_structure'])
-    log_dir = os.path.join(yses['session']['log_dir_parent'],
-                           yses['session']['log_dir_structure'])
+def read_configfile(configfile_path):
+    y = yaml.safe_load(open(configfile_path))
     
-
-    epo_obj_gen = arogen.EpochRange(yses['epoch_range']['epoch1'], 
-                                    yses['epoch_range']['epoch2'],
-                                    yses['epoch_range']['period'],
-                                    yses['epoch_range']['round_method'],
-                                    yses['epoch_range']['tz'])
+    y_station = y["station"]
     
-    workflow_lis = []
-    #### manage workflow
-    y_workflow =  yses['workflow']    
-    for k_step, ywkf in y_workflow.items():
+    y_site = y_station["site"]
+    y_device = y_station["device"]
+    y_access = y_station["access"]
+    
+    y_sessions_list = y_station["sessions_list"]
+    
+    for yses in y_sessions_list:
+        tmp_dir = os.path.join(yses['session']['tmp_dir_parent'],
+                               yses['session']['tmp_dir_structure'])
+        log_dir = os.path.join(yses['session']['log_dir_parent'],
+                               yses['session']['log_dir_structure'])
+        
+    
+        epo_obj_gen = arogen.EpochRange(yses['epoch_range']['epoch1'], 
+                                        yses['epoch_range']['epoch2'],
+                                        yses['epoch_range']['period'],
+                                        yses['epoch_range']['round_method'],
+                                        yses['epoch_range']['tz'])
+        
+        workflow_lis = []
+        #### manage workflow
+        y_workflow =  yses['workflow']    
+        for k_step, ywkf in y_workflow.items():
+                    
+            out_dir = os.path.join(ywkf['out_dir_parent'],
+                                   ywkf['out_dir_structure'])
+            
+            if k_step == 'download':
+                Dwl =  arodwl.DownloadGnss
+                dwl_obj = Dwl(out_dir=out_dir,
+                              tmp_dir=tmp_dir,
+                              log_dir=log_dir,
+                              epoch_range=epo_obj_gen,
+                              access=y_access,
+                              remote_dir=ywkf['remote_dir'],
+                              remote_fname=ywkf['remote_fname'],
+                              site=y_site,
+                              session=yses['session'])
                 
-        out_dir = os.path.join(ywkf['out_dir_parent'],
-                               ywkf['out_dir_structure'])
-        
-        if k_step == 'download':
-            Dwl =  arodwl.DownloadGnss
-            dwl_obj = Dwl(out_dir=out_dir,
-                          tmp_dir=tmp_dir,
-                          log_dir=log_dir,
-                          epoch_range=epo_obj_gen,
-                          access=y_access,
-                          remote_dir=ywkf['remote_dir'],
-                          remote_fname=ywkf['remote_fname'],
-                          site=y_site,
-                          session=yses['session'])
-            
-        if k_step == 'conversion':
-            Cnv = arocnv.ConvertRinexModGnss
-            cnv_obj = Cnv(out_dir=out_dir,
-                          tmp_dir=tmp_dir,
-                          log_dir=log_dir,
-                          epoch_range=epo_obj_gen,
-                          site=y_site,
-                          session=yses['session'])
-            
-
-
-            
-        
-        
-    
-
-
-
-
-
-
-
-    
+                workflow_lis.append(dwl_obj)
+                
+            if k_step == 'conversion':
+                Cnv = arocnv.ConvertRinexModGnss
+                cnv_obj = Cnv(out_dir=out_dir,
+                              tmp_dir=tmp_dir,
+                              log_dir=log_dir,
+                              epoch_range=epo_obj_gen,
+                              site=y_site,
+                              session=yses['session'])
+                
+                workflow_lis.append(cnv_obj)
+                
+                
+    return workflow_lis, y_site, y_device, y_access
+                
     
 ###################### FUNCTION GRAVEYARD
 
