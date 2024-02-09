cb1fba9492fea44ac0e67e23445b515195e3f60d
sysop@PitonHudson <sysop@PitonHudson.ipgp.fr>
2024-02-09 13:26:53 +0000

    correction on hudson

--- autorino/download/download_test_scripts/dlts_mk10_ovpf.py
+++ autorino/download/download_test_scripts/dlts_mk10_ovpf.py
@@ -12,9 +12,9 @@ import yaml
 
 ###############################################################################
 ## dummy comment2
-pcfg = '/home/sysop/pymods_ovs/autorino/configfiles/proto_config_06b_CFNG.yml'
+pcfg = '/home/sysop/pymods_ovs/autorino/configfiles/proto_config_06d_CFNG.yml'
 
 tup_out = arocfg.read_configfile(pcfg)
 workflow_lis, y_site, y_device, y_access = tup_out
-arocfg.workflow_run(workflow_lis)
+arocfg.run_workflow(workflow_lis)
 
