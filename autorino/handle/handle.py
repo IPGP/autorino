ea1511668bed257b1400822fcf2b2c8ece24dd0f
PSTPX1 <psakicki@TPX1-GFZ>
2024-02-29 18:40:06 +0100

    new split prototype

--- autorino/handle/handle.py
+++ autorino/handle/handle.py
@@ -5,39 +5,91 @@ Created on Wed Jan 10 15:00:40 2024
 
 @author: psakic
 """
+import numpy as np
 
 import autorino.common as arocmn
 import autorino.convert as arocnv
 
+import pandas as pd
+
 # Create a logger object.
 import logging
+
 logger = logging.getLogger(__name__)
 
+
 class HandleGnss(arocmn.StepGnss):
-    def __init__(self,out_dir,tmp_dir,log_dir,
+    def __init__(self, out_dir, tmp_dir, log_dir,
                  epoch_range,
                  site=None,
                  session=None):
-    
-        super().__init__(out_dir,tmp_dir,log_dir,
+
+        super().__init__(out_dir, tmp_dir, log_dir,
                          epoch_range,
                          site=site,
                          session=session)
 
+    def divide_by_epochs(self,
+                         period='1d',
+                         rolling_period=False,
+                         rolling_ref=-1,
+                         round_method='floor',
+                         drop_epoch_rnd=False):
+
+        epoch_rnd = arocmn.round_epochs(self.table['epoch_srt'],
+                                        period=period,
+                                        rolling_period=rolling_period,
+                                        rolling_ref=rolling_ref,
+                                        round_method=round_method)
+
+        self.table['epoch_rnd'] = epoch_rnd
+
+        # get individual Handle objects
+        grps = self.table.groupby('epoch_rnd')
+
+        stp_obj_lis_out = []
+
+        for tgrp, tabgrp in grps:
+            hdl_obj = self.copy()
+
+            if drop_epoch_rnd:
+                tabgrp_bis = tabgrp.drop('epoch_rnd', axis=1)
+            else:
+                tabgrp_bis = pd.DataFrame(tabgrp)
+            hdl_obj.table = tabgrp_bis
+            hdl_obj.update_epoch_range_from_table()
+            stp_obj_lis_out.append(hdl_obj)
+
+        # get the main Handle object which will describe the final spliced RINEXs
+        hdl_main_obj = self.copy()
+
+        # hdl_main_obj_epoch_range = arocmn.EpochRange(np.min(epoch_rnd),
+        #                                              np.max(epoch_rnd),
+        #                                              period=period,
+        #                                              round_method=round_method)
+        #
+        # hdl_main_obj = HandleGnss(out_dir=self.out_dir,
+        #                           tmp_dir=self.tmp_dir,
+        #                           log_dir=self.log_dir,
+        #                           epoch_range=hdl_main_obj_epoch_range,
+        #                           site=self.site,
+        #                           session=self.session)
+        return stp_obj_lis_out
+
     def splice(self):
-        ### divide_step_by_epochs will create several HandleGnss objects
-        hdl_objs_lis = self.divide_step_by_epochs()
-        
+        ### divide_by_epochs will create several HandleGnss objects
+        hdl_objs_lis = self.divide_by_epochs()
+
         for hdl in hdl_objs_lis:
             hdl._splice_mono()
-        
-    def _splice_mono(self):
+
+    def splice_mono(self, handle_software='converto'):
         #### add a test here to be sure that only one epoch is inside
         fpath_inp_lst = list(self.table['fpath_inp'])
-        
-        tmp_dir_use = self.translate_path(self.tmp_dir)
 
-        handle_software = 'converto'
+        tmp_dir_use = self.translate_path(self.tmp_dir)
+        out_dir_use = self.translate_path(self.out_dir)
+
         if handle_software == 'converto':
             frnxtmp, _ = arocnv.converter_run(fpath_inp_lst,
                                               tmp_dir_use,
@@ -49,15 +101,3 @@ class HandleGnss(arocmn.StepGnss):
                                               'gfzrnx',
                                               bin_options=['-f'])
 
-
-
-
-    # def find_rnx_
-
-    # def split(self):
-
-    
-            
-    
-
-        
