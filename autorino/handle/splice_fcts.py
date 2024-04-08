086fcb346c5e2aafcce1c4f077b90943cb753b42
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-08 18:47:23 +0200

    splice before in row refactor

---
+++ autorino/handle/splice_fcts.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import autorino.common as arocmn
+import autorino.handle as arohdl
+
+import os
+
+def splice_rnx(rnxs_inp,
+               tmp_dir,out_dir,
+               handle_software='converto',
+               period='1d',
+               rolling_period=False,
+               rolling_ref=-1,
+               round_method='floor',
+               drop_epoch_rnd=False):
+
+    ### define other dirs
+    rnxmod_dir = os.path.join(tmp_dir,'rinexmoded')
+    log_dir = tmp_dir
+
+    #### define spc_inp: the Splice object which will store all the input RINEXs
+    spc_inp = arohdl.SpliceGnss(out_dir, tmp_dir, log_dir)
+    spc_inp.load_table_from_filelist(rnxs_inp)
+    spc_inp.update_epoch_table_from_rnx_fname(use_rnx_filename_only=True)
+
+    spc_out = spc_inp.splice(period=period,
+                             rolling_period=rolling_period,
+                             rolling_ref=rolling_ref,
+                             round_method=round_method,
+                             drop_epoch_rnd=drop_epoch_rnd)
+
+    return spc_out
