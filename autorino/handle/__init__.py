79c5b107b11d90a971b5799d84562f9004cd16f6
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-08 18:47:23 +0200

    splice before in row refactor

--- autorino/handle/__init__.py
+++ autorino/handle/__init__.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from .splice_cls import *
-from .split_cls  import *
+from .splice_cls  import *
+from .splice_fcts import *
+from .split_cls   import *
 from .split_fcts  import *
 
 
