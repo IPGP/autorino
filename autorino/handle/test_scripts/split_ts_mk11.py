f7d2b41f3232814c6319a54f1b9d2770c6a64510
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-08 15:48:24 +0200

    clean split/splice

--- autorino/handle/test_scripts/split_ts_mk11.py
+++ autorino/handle/test_scripts/split_ts_mk11.py
@@ -23,6 +23,10 @@ epo = arocmn.EpochRange(dt.datetime(2024, 2, 28, 1),
                         dt.datetime(2024, 2, 28, 3),
                         '5min')
 
+epo = (dt.datetime(2024, 2, 28, 1),
+       dt.datetime(2024, 2, 28, 3),
+       '5min')
+
 p = "/home/psakicki/autorino_workflow_tests/conv_tests/CFNG00REU/2024"
 L = utils.find_recursive(p, "*gz")
 
