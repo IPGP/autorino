da7d775220c92443ec801fff0cc15fb27acef518
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-08 15:48:24 +0200

    clean split/splice

--- autorino/handle/split_fcts.py
+++ autorino/handle/split_fcts.py
@@ -4,25 +4,25 @@
 import autorino.common as arocmn
 import autorino.handle as arohdl
 
-from geodezyx import utils
-import datetime as dt
 import os
 
-def split_rnx(epo_in,rnxs_inp,tmp_dir,out_dir,
+def split_rnx(epo_inp,
+              rnxs_inp,tmp_dir,out_dir,
               handle_software='converto'):
 
     ### define other dirs
     rnxmod_dir = os.path.join(tmp_dir,'rinexmoded')
     log_dir = tmp_dir
 
-    #### define spt_store: the Split object which will store the input RINEXs
+    #### define spt_store: the Split object which will store all the input RINEXs
     epo_dummy = arocmn.create_dummy_epochrange()
-    spt_store = arohdl.SplitGnss(out_dir, tmp_dir, log_dir, epo_dummy)  # , site_id='CFNG')
+    spt_store = arohdl.SplitGnss(out_dir, tmp_dir, log_dir)
     spt_store.load_table_from_filelist(rnxs_inp)
     spt_store.update_epoch_table_from_rnx_fname(use_rnx_filename_only=True)
 
     #### define spt_split: the Split object which will perform the split operation
-    spt_split = arohdl.SplitGnss(out_dir, tmp_dir, log_dir, epo)
+    spt_split = arohdl.SplitGnss(out_dir, tmp_dir, log_dir, epo_inp)
     spt_split.find_rnxs_for_split(spt_store)
     spt_split.decompress()
     spt_split.split(handle_software=handle_software)
+
