e6f886239bc87427da0135d35a73586e4d4ea1c2
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-11 17:51:16 +0200

    refactor eporng

--- autorino/common/__init__.py
+++ autorino/common/__init__.py
@@ -1,5 +1,5 @@
-from .epoch_range_cls  import *
-from .epoch_range_fcts import *
+from .eporng_cls       import *
+from .eporng_fcts      import *
 from .step_cls         import *
 from .step_fcts        import *
 from .translate        import *
