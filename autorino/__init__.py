daac1916becc1015c2a4f8461e74a5a03696188b
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-17 17:59:13 +0200

    init comment

--- autorino/__init__.py
+++ autorino/__init__.py
@@ -5,16 +5,14 @@ import os
 from os import path
 import logging.config
 
+
+
 #### IMPORT CONFIG FOR LOGGER
 log_file_path = os.path.join(path.dirname(path.abspath(__file__)),
                              'config',
                              'logcfg.py')
 from . import config
 if os.path.isfile(log_file_path):
-    ##print("INFO:",log_file_path,"found")
-    ###### old config file format
-    ###logging.config.fileConfig(fname=log_file_path, disable_existing_loggers=False)
-    ###### new dict
     logging.config.dictConfig(config.logcfg.log_config_dict)
 else:
     print("ERR:logger config file",log_file_path,"is missing")
