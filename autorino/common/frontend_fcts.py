bb700e23c55159e3c73b880e208d720aacdd10c3
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-23 17:49:46 +0200

    split + splice > handle class (again)

--- autorino/common/frontend_fcts.py
+++ autorino/common/frontend_fcts.py
@@ -28,43 +28,48 @@ def convert_rnx(rnxs_inp, tmp_dir, out_dir, log_dir=None,
 
     return None
 
-def split_rnx(rnxs_inp,epo_inp,tmp_dir,out_dir,log_dir,
-              handle_software='converto',
-              rinexmod_options=None):
 
+def split_rnx(rnxs_inp, epo_inp, tmp_dir, out_dir, log_dir = None,
+              handle_software='converto',
+              rinexmod_options=None,
+              metadata=None):
     ### define other dirs
     if not log_dir:
         log_dir = tmp_dir
 
     #### define spt_store: the Split object which will store all the input RINEXs
-    spt_store = arohdl.SplitGnss(out_dir, tmp_dir, log_dir)
+    spt_store = arohdl.HandleGnss(out_dir, tmp_dir, log_dir,
+                                  metadata=metadata)
     spt_store.load_table_from_filelist(rnxs_inp)
     spt_store.update_epoch_table_from_rnx_fname(use_rnx_filename_only=True)
 
     #### define spt_split: the Split object which will perform the split operation
-    spt_split = arohdl.SplitGnss(out_dir, tmp_dir, log_dir, epo_inp)
+    spt_split = arohdl.HandleGnss(out_dir, tmp_dir, log_dir, epo_inp,
+                                  metadata=metadata)
     spt_split.find_rnxs_for_handle(spt_store)
 
     spt_split.split(handle_software=handle_software,
                     rinexmod_options=rinexmod_options)
     return None
 
+
 def splice_rnx(rnxs_inp,
-               tmp_dir,out_dir,log_dir=None,
+               tmp_dir, out_dir, log_dir=None,
                handle_software='converto',
                period='1d',
                rolling_period=False,
                rolling_ref=-1,
                round_method='floor',
                drop_epoch_rnd=False,
-               rinexmod_options=None):
-
+               rinexmod_options=None,
+               metadata=None):
     ### define other dirs
     if not log_dir:
         log_dir = tmp_dir
 
     #### define spc_inp: the Splice object which will store all the input RINEXs
-    spc_inp = arohdl.SpliceGnss(out_dir, tmp_dir, log_dir)
+    spc_inp = arohdl.HandleGnss(out_dir, tmp_dir, log_dir,
+                                metadata=metadata)
     spc_inp.load_table_from_filelist(rnxs_inp)
     spc_inp.update_epoch_table_from_rnx_fname(use_rnx_filename_only=True)
 
