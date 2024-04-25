7b09808cbe7b9beb4d969e186d0bd3f50567d0c8
PSTPX1 <psakicki@TPX1-GFZ>
2024-04-25 15:28:03 +0200

    change env file

--- autorino/config/env_read.py
+++ autorino/config/env_read.py
@@ -16,20 +16,27 @@ def read_env(envfile_path=None):
     """
     read a environement config file path (YAML format) and return
     the corresponding dictionnary
+
+    priority for envfile path :
+    fct argument > bashrc env variable > default in the current file
     """
 
     envfile_path_def = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                     "autorino_env_default.yml")
-    if not envfile_path:
-        envfile_path_use = envfile_path_def
-    elif not os.path.isfile(envfile_path):
-        logger.warning("environement config file not found, fallback to default values: %s",
-                       envfile_path)
-        envfile_path_use = envfile_path_def
-    else:
+
+    if envfile_path:
         logger.debug("load environement config file: %s",
                      envfile_path)
         envfile_path_use = envfile_path
+    elif 'AUTORINO_ENV' in os.environ:
+        envfile_path_use = os.environ['AUTORINO_ENV']
+    else:
+        envfile_path_use = envfile_path_def
+
+    if not os.path.isfile(envfile_path_use):
+        logger.warning("environement config file not found, fallback to default values: %s",
+                       envfile_path_use)
+        envfile_path_use = envfile_path_def
 
     y_dic = yaml.safe_load(open(envfile_path_use))
 
