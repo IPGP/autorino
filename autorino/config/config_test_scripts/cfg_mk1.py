714eecb8ba71a4a63c2eb7fcc0ce17579b67cf15
PSTPX1 <psakicki@TPX1-GFZ>
2024-05-29 15:33:13 +0200

    changes on tpx1

--- autorino/config/config_test_scripts/cfg_mk1.py
+++ autorino/config/config_test_scripts/cfg_mk1.py
@@ -12,8 +12,18 @@ import pandas as pd
 import autorino.common as arocmn
 import autorino.config as arocfg
 
+
+
+
 pmain = '/home/psakicki/CODES/IPGP/autorino/configfiles/main/autorino_main_cfg_01a.yml'
 psite = '/home/psakicki/CODES/IPGP/autorino/configfiles/sites/autorino_site_cfg_08a_CFNG.yml'
+
+pmain = '/home/psakicki/CODES/IPGP/autorino_configfiles/configfiles/OVPF/main/autorino_main_cfg_01a.yml'
+psite = '/home/psakicki/CODES/IPGP/autorino_configfiles/configfiles/OVPF/sites/autorino_site_cfg_08a_FOAG.yml'
+
+
+
+# CFNG
 
 #out = arocfg.read_configfile_sessions_list(psite)
 out = arocfg.read_cfg(psite, main_cfg_path=pmain)
