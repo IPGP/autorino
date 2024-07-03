1ab53fd4dff966983d741773c2676e3754dfe493
PSTPX1 <psakicki@TPX1-GFZ>
2024-07-03 09:51:55 +0200

    env priority + doc string splice

--- autorino/config/env_read.py
+++ autorino/config/env_read.py
@@ -12,7 +12,7 @@ import os
 import yaml
 
 logger = logging.getLogger(__name__)
-logger.setLevel("INFO")
+logger.setLevel("DEBUG")
 
 
 def read_env(envfile_path=None):
@@ -27,17 +27,22 @@ def read_env(envfile_path=None):
     envfile_path_def = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                     "autorino_env_default.yml")
 
+    envfile_path_use = None
+
     if envfile_path:
         envfile_path_use = envfile_path
     elif 'AUTORINO_ENV' in os.environ:
         envfile_path_use = os.environ['AUTORINO_ENV']
     else:
-        envfile_path_use = envfile_path_def
+        envfile_path_use = ""
 
-    if not os.path.isfile(envfile_path_use):
-        if not envfile_path_use is None:
-            logger.warning("custom environement config file not found: %s, fallback to default values",
-                           envfile_path_use)
+    if not os.path.isfile(envfile_path_use) or envfile_path_use == "":
+        if envfile_path_use == "":
+            logger.warning("custom environement config file not defined in the environnement variable AUTORINO_ENV")
+        else:
+            logger.warning("custom environement config file not found in %s",envfile_path_use)
+
+        logger.warning("fallback to default values in %s",envfile_path_def)
         envfile_path_use = envfile_path_def
 
     logger.debug("load environement config file: %s",envfile_path_use)
