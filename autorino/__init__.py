3cbffdd814a5749b0e2b800ae885952d26c1a659
PSTPX1 <psakicki@TPX1-GFZ>
2024-07-31 12:28:03 +0200

    correct init from bad nores commit

--- autorino/__init__.py
+++ autorino/__init__.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
-import os
-from os import remove, close, path
-import logging
-import logging.config
-
-#### IMPORT CONFIG FOR LOGGER
-log_file_path = os.path.join(path.dirname(path.abspath(__file__)),'logconfig','logautorino.py')
-from . import logconfig
-if os.path.isfile(log_file_path):
-    ##print("INFO:",log_file_path,"found")
-    ###### old config file format
-    ###logging.config.fileConfig(fname=log_file_path, disable_existing_loggers=False)
-    ###### new dict
-    logging.config.dictConfig(logconfig.logautorino.log_config_dict)
-=======
 import logging.config
 import os
 from os import path
@@ -28,6 +12,5 @@ log_file_path = os.path.join(path.dirname(path.abspath(__file__)),
 from . import config
 if os.path.isfile(log_file_path):
     logging.config.dictConfig(config.logcfg.log_config_dict)
->>>>>>> afa732e2589f03e3e073da617496f194c770d857
 else:
     print("ERR:logger config file",log_file_path,"is missing")
