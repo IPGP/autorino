d000e0cd0be4a6997fa419dba551346cf3422ea9
PSTPX1 <psakicki@TPX1-GFZ>
2024-08-02 10:22:16 +0200

    load def env values

--- autorino/config/env_read.py
+++ autorino/config/env_read.py
@@ -24,33 +24,49 @@ def read_env(envfile_path=None):
     fct argument > bashrc env variable > default in the current file
     """
 
-    envfile_path_def = os.path.join(os.path.dirname(os.path.abspath(__file__)),
-                                    "autorino_env_default.yml")
+    # Set the default path for the environment file
+    envfile_path_def = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "autorino_env_default.yml"
+    )
 
+    # Initialize the variable to hold the path to the environment file to be used
     envfile_path_use = None
 
+    # Determine the environment file path based on the function argument,
+    # environment variable, or default to an empty string
     if envfile_path:
         envfile_path_use = envfile_path
-    elif 'AUTORINO_ENV' in os.environ:
-        envfile_path_use = os.environ['AUTORINO_ENV']
+    elif "AUTORINO_ENV" in os.environ:
+        envfile_path_use = os.environ["AUTORINO_ENV"]
     else:
         envfile_path_use = ""
 
+    # Check if the specified environment file exists, otherwise fallback to the default file
     if not os.path.isfile(envfile_path_use) or envfile_path_use == "":
         if envfile_path_use == "":
-            logger.warning("custom environement config file not defined in the environnement variable AUTORINO_ENV")
+            logger.warning(
+                "custom environment config file not defined in the environment variable AUTORINO_ENV"
+            )
         else:
-            logger.warning("custom environement config file not found in %s",envfile_path_use)
+            logger.warning(
+                "custom environment config file not found in %s", envfile_path_use
+            )
 
-        logger.warning("fallback to default values in %s",envfile_path_def)
+        logger.warning("fallback to default values in %s", envfile_path_def)
         envfile_path_use = envfile_path_def
 
-    logger.debug("load environement config file: %s",envfile_path_use)
+    # Log the path of the environment file being loaded
+    logger.debug("load environment config file: %s", envfile_path_use)
 
-    y_dic = yaml.safe_load(open(envfile_path_use))
+    # Load the default and specified environment files and merge their contents
+    env_dic_def = yaml.safe_load(open(envfile_path_def))
+    env_dic_use = yaml.safe_load(open(envfile_path_use))
 
-    return y_dic
+    env_dic_fin = env_dic_def.copy()
+    env_dic_fin.update(env_dic_use)
+
+    # Return the merged dictionary
+    return env_dic_fin
+
 
 aro_env_dict = read_env()
-
-
