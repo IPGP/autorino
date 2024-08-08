b1b98b891aad5c83375e9b88e745fbed435b3fc6
PSTPX1 <psakicki@TPX1-GFZ>
2024-08-08 17:41:59 +0200

    trm conv selected

--- autorino/convert/cnv_cmd_run.py
+++ autorino/convert/cnv_cmd_run.py
@@ -93,7 +93,7 @@ def _convert_select(converter_inp, inp_raw_fpath=None):
 
     # +++++ TRIMBLE
     # preliminary tests: Trimble default converter must be extracted from the environment values
-    if ext == (".T00", ".T00", ".T02", ".T04") and converter_inp == "auto":
+    if ext in (".T00", ".T00", ".T02", ".T04") and converter_inp == "auto":
         converter_inp = aroenv.aro_env_dict["general"]["trimble_default_software"]
         logger.debug("Trimble default converter defined in environnement: %s", converter_inp)
 
