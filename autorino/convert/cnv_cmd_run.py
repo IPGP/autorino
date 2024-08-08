472c0eead99f112d862482700a2f7d478128e812
PSTPX1 <psakicki@TPX1-GFZ>
2024-08-08 17:26:58 +0200

    trm conv selected

--- autorino/convert/cnv_cmd_run.py
+++ autorino/convert/cnv_cmd_run.py
@@ -92,7 +92,11 @@ def _convert_select(converter_inp, inp_raw_fpath=None):
         fname = inp_raw_fpath.name.upper()
 
     # +++++ TRIMBLE
-    if ext in (".T00", ".T02", ".T04") or converter_inp == "t0xconvert":
+    # preliminary tests: Trimble default converter must be extracted from the environment values
+    if ext == (".T00", ".T00", ".T02", ".T04") and converter_inp == "auto":
+        converter_inp = aroenv.aro_env_dict["general"]["log_level"]
+
+    if ext in (".T00", ".T00", ".T02", ".T04") or converter_inp == "t0xconvert":
         converter_name = "t0xconvert"
         brand = "Trimble (official converter)"
         cmd_build_fct = arcv.cmd_build_t0xconvert
@@ -100,7 +104,7 @@ def _convert_select(converter_inp, inp_raw_fpath=None):
         bin_options = []
         bin_kwoptions = dict()
 
-    elif ext in (".T00", ".T02", ".T04") or converter_inp == "trm2rinex":
+    elif ext in (".T00", ".T00", ".T02", ".T04") or converter_inp == "trm2rinex":
         converter_name = "trm2rinex"
         brand = "Trimble (unofficial converter)"
         cmd_build_fct = arcv.cmd_build_trm2rinex
@@ -108,7 +112,7 @@ def _convert_select(converter_inp, inp_raw_fpath=None):
         bin_options = []
         bin_kwoptions = dict()
 
-    elif ext == ".T02" or converter_inp == "runpkr00":
+    elif ext == (".T00", ".T00", ".T02", ".T04") or converter_inp == "runpkr00":
         converter_name = "runpkr00"
         brand = "Trimble (legacy converter)"
         cmd_build_fct = arcv.cmd_build_runpkr00
