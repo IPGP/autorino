ab29161eb885f93406a501af72a0226432bfa6d7
PSTPX1 <psakicki@TPX1-GFZ>
2024-08-20 17:40:41 +0200

    cosmetic

--- autorino/convert/cnv_cls.py
+++ autorino/convert/cnv_cls.py
@@ -192,7 +192,7 @@ class ConvertGnss(arocmn.StepGnss):
             prv_tbl_df = arocmn.load_previous_tables(self.tmp_dir_logs)
             # Filter previous tables stored in log_dir
             if len(prv_tbl_df) > 0:
-                self.filter_previous_tables(prv_tbl_df)
+                self.filter_prev_tab(prv_tbl_df)
             self.filter_ok_out()
 
         self.tmp_decmp_files, _ = self.decompress()
