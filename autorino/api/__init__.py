5f94bd03899bdfd0926a0ab9524fed24867e67c7
PSTPX1 <psakicki@TPX1-GFZ>
2024-09-18 18:30:26 +0400

    cfgfile run rename no more autorino

--- autorino/api/__init__.py
+++ autorino/api/__init__.py
@@ -7,7 +7,7 @@ Created on 18/09/2024 18:13:25
 """
 
 from .api_fcts import *
-from .autorino_configfile_run import *
+from .configfile_run import *
 from .convert_rnx import *
 from .download_raw import *
 from .splice_rnx_abs import *
