3baf62e52e44e89d3556fde8be6b59393f93c1c2
PSTPX1 <psakicki@TPX1-GFZ>
2024-10-31 18:10:57 +0100

    better TZ

--- autorino/cfgfiles/cfgfile_read.py
+++ autorino/cfgfiles/cfgfile_read.py
@@ -465,11 +465,11 @@ def run_steps(steps_lis, steps_select_list=None, exclude_steps_select=False, pri
             stp.convert(**stp.options)
         elif stp.get_step_type() == "splice":
             stp_rnx_inp = stp.copy()
-            stp_rnx_inp.load_tab_inpdir()
+            stp_rnx_inp.load_tab_inpdir(update_epochs=True)
             stp.splice(input_mode="given", input_rinexs=stp_rnx_inp, **stp.options)
         elif stp.get_step_type() == "split":
             stp_rnx_inp = stp.copy()
-            stp_rnx_inp.load_tab_inpdir()
+            stp_rnx_inp.load_tab_inpdir(update_epochs=True)
             stp.split(input_mode="given", input_rinexs=stp_rnx_inp, **stp.options)
 
     return None
