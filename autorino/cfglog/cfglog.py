5e50cb0387990c835fabe59bbf3e218e7d8b6835
PSTPX1 <psakicki@TPX1-GFZ>
2025-01-04 21:29:03 +0100

    debug logger

--- autorino/cfglog/cfglog.py
+++ autorino/cfglog/cfglog.py
@@ -8,7 +8,7 @@
 log_config_dict = {
     "version": 1,
     "disable_existing_loggers": False,
-    "root": {"handlers": ["console_root"], "level": "WARN", "propagate": True},
+    "root": {"handlers": ["console_gyxz"], "level": "WARN", "propagate": True},
     "loggers": {
         "autorino": {  # THIS KEY MUST DESCRIBES THE CURRENT MODULE!!!!
             "handlers": ["console_gyxz"],
@@ -23,7 +23,7 @@ log_config_dict = {
     },
     "handlers": {
         "console_root": {
-            #            "formatter":"fmtgzyx1",
+            # "formatter":"fmtgzyx1",
             "class": "logging.StreamHandler",
             "level": "DEBUG",
         },
