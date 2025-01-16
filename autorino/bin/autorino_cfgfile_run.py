b40f2ff839e11be13155cedf913b4ae329dff9b4
PSTPX1 <psakicki@TPX1-GFZ>
2025-01-15 20:11:14 -0400

    better doc

--- autorino/bin/autorino_cfgfile_run.py
+++ autorino/bin/autorino_cfgfile_run.py
@@ -13,7 +13,7 @@ def main():
                 "    autorino_cfgfile_run -c site_cfg.yml -s 2025-01-01 -e '10 days ago'\n"
                 "  * run download and convert only for HOUZ00GLP & BORG00REU sites only:\n"
                 "    autorino_cfgfile_run -c cfgfiles_dir -m main_cfg.yml -ls HOUZ00GLP,BORG00REU -ss download,convert"),
-        #formatter_class = argparse.RawDescriptionHelpFormatter,
+        formatter_class = argparse.RawDescriptionHelpFormatter,
 
     )
 
