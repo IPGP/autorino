65ca26f74992ecb3fe9f1972515f5c177adfa207
PSTPX1 <psakicki@TPX1-GFZ>
2025-01-27 14:57:44 -0400

    correct read files

--- autorino/bin/autorino_convert_rnx.py
+++ autorino/bin/autorino_convert_rnx.py
@@ -15,13 +15,13 @@ def main():
     parser.add_argument(
         "raws_inp",
         nargs="+",
-        help="The input RAW files to be converted"
+        help="The input RAW files to be converted "
         "Possible inputs are: \n"
-        " * one single RAW file path \n"
-        " * a list of RAW path \n"
-        " * a text file containing a list of RAW paths \n"
-        " (then --list_file_input must be activated) \n"
-        " * a directory containing RAW files \n",
+        "* one single RAW file path \n"
+        "* a comma-separated (,) list of RAW paths \n"
+        "* a text file containing a list of RAW paths \n"
+        "(then --list_file_input must be activated) \n"
+        "* a directory containing RAW files \n",
     )
     parser.add_argument(
         "out_dir", help="The output directory where the converted files will be stored"
@@ -36,45 +36,46 @@ def main():
     parser.add_argument(
         "-s",
         "--out_structure",
-        help="The structure of the output directory."
+        help="The structure of the output directory. "
              "If provided, the converted files will be "
-             "stored in a subdirectory of out_dir following this structure."
-             "See README.md for more information."
-             "Typical values are '<SITE_ID4>/%%Y/' or '%%Y/%%j/",
+             "stored in a subdirectory of out_dir following this structure. "
+             "See README.md for more information. "
+             "Typical values are '<SITE_ID4>/%%Y/' or '%%Y/%%j/.",
         default="<SITE_ID4>/%Y/",
     )
     parser.add_argument(
         "-tmp",
         "--tmp_dir",
         help="The temporary directory used during the conversion process. "
-             "If not provided, it defaults to <out_dir>/tmp_convert_rnx.",
+             "If not provided, it defaults to <out_dir>/tmp_convert_rnx. ",
         default=None,
     )
     parser.add_argument(
         "-log",
         "--log_dir",
-        help="The directory where logs will be stored."
-             "If not provided, it defaults to tmp_dir",
+        help="The directory where logs will be stored. "
+             "If not provided, it defaults to tmp_dir ",
         default=None,
     )
     parser.add_argument(
         "-rimo",
         "--rinexmod_options",
         type=yaml.safe_load,
-        help="The options for modifying the RINEX files during the conversion."
-             "The options must be provided in a dictionnary represented as a string"
-             "e.g. '{longname: False, filename_style: basic}'"
+        help="The options for modifying the RINEX files during the conversion. "
+             "The options must be provided in a dictionnary represented as a string "
+             "e.g. '{longname: False, filename_style: basic}' "
              "Defaults to None",
     )
     parser.add_argument(
         "-m",
         "--metadata",
-        help="""The metadata to be included in the converted RINEX files. \n Possible inputs are: \n 
-                             * list of string (sitelog file paths), 
-                             * single string (single sitelog file path),
-                             * single string (directory containing the sitelogs),
-                             * list of MetaData objects,
-                             * single MetaData object""",
+        help="""
+        The metadata to be included in the converted RINEX files. \n 
+        Possible inputs are: \n 
+        * list of string (sitelog file paths)  
+        * single string (single sitelog file path) 
+        * single string (directory containing the sitelogs)
+        """,
     )
     parser.add_argument(
         "-f",
@@ -88,7 +89,7 @@ def main():
 
     if args.list_file_input:
         with open(args.raws_inp[0], "r") as f:
-            raws_inp = f.splitlines()
+            raws_inp = f.read().splitlines()
     else:
         raws_inp = args.raws_inp
 
