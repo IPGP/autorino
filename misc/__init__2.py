418f393873d71324da6c18f2334e56fbaf3f2dd1
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-04-08 15:09:09 +0200

    new init

---
+++ misc/__init__2.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 07/04/2025 15:01:05
+
+@author: psakic
+"""
+
+import logging.config
+import os
+from os import path
+import json
+
+__version__='1.2.0' # changed automaticcaly with bump-my-version
+
+#### IMPORT CONFIG FOR LOGGER
+log_json_path = os.path.join(
+    path.dirname(path.abspath(__file__)), "cfglog", "cfglog.json"
+)
+
+if os.path.isfile(log_json_path):
+    with open(log_json_path) as f:
+        json_config = json.load(f)
+    logging.config.dictConfig(json_config)
+else:
+    print("ERR:logger cfgfiles file", log_json_path, "is missing")
+
+#### IMPORT AUTORINO INTERNAL SUBMODULES
+from . import api
+from . import bin
+from . import cfgenv
+from . import cfgfiles
+from . import cfglog
+from . import check
+from . import common
+from . import convert
+from . import download
+from . import handle
+
+__all__ = ['api',
+           'bin',
+           'cfgenv',
+           'cfgfiles',
+           'cfglog',
+           'check',
+           'common',
+           'convert',
+           'download',
+           'handle']
+
+if __name__ == '__main__':
+    print("autorino version",__version__)
+    print("autorino.__all__",__all__)
