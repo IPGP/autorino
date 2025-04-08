3664b38ef9dd65a37df2da4d41a76aa53f578be8
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-04-08 15:43:52 +0200

    add __init__.py with module imports and version information

---
+++ autorino/__init__.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on 07/04/2025 15:01:05
+
+@author: psakic
+"""
+
+#### IMPORT AUTORINO INTERNAL SUBMODULES
+from . import api
+from . import cli
+from . import cfgenv
+from . import cfgfiles
+from . import cfglog
+from . import check
+from . import common
+from . import convert
+from . import download
+from . import handle
+
+__all__ = ['api',
+           'cli',
+           'cfgenv',
+           'cfgfiles',
+           'cfglog',
+           'check',
+           'common',
+           'convert',
+           'download',
+           'handle']
+
+if __name__ == '__main__':
+    print("autorino version",__version__)
+    print("autorino.__all__",__all__)
