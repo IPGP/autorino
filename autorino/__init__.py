65f77690e36152938149ede21e94fb85a69495f7
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-04-08 15:05:29 +0200

    update keywords in pyproject.toml for better relevance and comment out optional dependencies

--- autorino/__init__.py
+++ autorino/__init__.py
@@ -26,7 +26,6 @@ else:
     print("ERR:logger cfgfiles file", log_json_path, "is missing")
 
 #### IMPORT AUTORINO INTERNAL SUBMODULES
-# necessary for frontend CLI programs
 from . import api
 from . import bin
 from . import cfgenv
