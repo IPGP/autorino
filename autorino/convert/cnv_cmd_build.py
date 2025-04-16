b66e033c17b76a5a5ceefe377c90e21c06e58a40
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-04-16 15:20:37 +0200

    add SNR for sbf2rin

--- autorino/convert/cnv_cmd_build.py
+++ autorino/convert/cnv_cmd_build.py
@@ -629,7 +629,7 @@ def cmd_build_sbf2rin(
     out_fpath = out_dir.joinpath(inp_raw_fpath.name + ".rnx_sbf2rin")
 
     cmd_list = (
-        [bin_path, "-f", inp_raw_fpath, "-o", out_fpath] + cmd_opt_list + cmd_kwopt_list
+        [bin_path, "-f", inp_raw_fpath, "-o", out_fpath, "-s"] + cmd_opt_list + cmd_kwopt_list
     )
     cmd_list = [str(e) for e in cmd_list]
     cmd_str = " ".join(cmd_list)
