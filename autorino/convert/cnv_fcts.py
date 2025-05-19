0d42adebf292f293d90d031074f826c438570bac
PiSa on TPX1 <psakicki@TPX1-GFZ>
2025-05-19 16:56:01 +0400

    update site identifier references in cnv_fcts.py

--- autorino/convert/cnv_fcts.py
+++ autorino/convert/cnv_fcts.py
@@ -61,9 +61,9 @@ def site_list_from_metadata(metadata_inp):
         metadata = metadata_inp
 
     # get the site (4chars) as a list
-    site4_list = [s.site4char for s in metadata]
+    site4_list = [s.site_id4 for s in metadata]
     # get the site (9chars) as a list
-    # site9_list = [s.site9char for s in metadata]
+    # site9_list = [s.site_id9 for s in metadata]
 
     site9_list = []
 
